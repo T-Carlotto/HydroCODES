c3a9a12f7c8470571abd6539c3a7e9ace53886ff
labhidro <thomas.carl@hotmail.com>
2022-11-10 14:19:18 -0300

    commit5

--- main.py
+++ main.py
@@ -10,23 +10,9 @@ import matplotlib.pylab as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 import csv
-import streamlit.components.v1 as components
+
 
 st.title("Hydrograph Separation Tool")
-
-components.html("""<form action="https://www.paypal.com/donate" method="post" target="_top">
-                    <!-- Identify your business so that you can collect the payments. -->
-                    <input type="hidden" name="business" value="donations@kcparkfriends.org">
-                    <!-- Specify details about the contribution -->
-                    <input type="hidden" name="no_recurring" value="0">
-                    <input type="hidden" name="item_name" value="Friends of the Park">
-                    <input type="hidden" name="item_number" value="Fall Cleanup Campaign">
-                    <input type="hidden" name="currency_code" value="USD">
-                    <!-- Display the payment button. -->
-                    <input type="image" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="Donate">
-                    <img alt="" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >
-                    </form> 
-                    """)
 
 tab1, tab2 = st.tabs(["🗃 Input data", "📈 Output"])
 
